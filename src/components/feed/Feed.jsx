298c3b14740a3972c111aa36eac3a30b54efdac7
MoizZakir <moizzakir87@gmail.com>
2024-03-05 19:07:51 +0500

    'newUpdate'

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
 import './feed.css'
+import { IoMdPhotos } from "react-icons/io";
+import { PiTagSimpleFill } from "react-icons/pi";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdEmojiEmotions } from "react-icons/md";
+
+
 export const Feed = () => {
   return (
-    <div className='feed'>Feed</div>
+    <div className='feed'>
+
+
+
+      <div className="feedActivities">
+        <div className="postCreate">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEUZrYHlA1Omsmisn1UTL18o4pY-X1c6Jmlw&usqp=CAU" alt="" />
+          <input type="text" placeholder="what's on your mind?" />
+        </div>
+          <hr/>
+          <div className='postFeeling'>
+            <div className='feelingItems'>
+            <IoMdPhotos fill='pink'/> <span>Photos</span>
+            </div>
+            <div className='feelingItems'>
+            <PiTagSimpleFill fill='blue'/> <span>Tags</span>
+            </div>
+            <div className='feelingItems'>
+            <FaLocationDot fill='green'/> <span>Location</span>
+            </div>
+            <div className='feelingItems'>
+            <MdEmojiEmotions fill='yellow'/> <span>Feelings</span>
+            </div>
+            
+            <button>Share</button>
+
+          </div>
+
+
+
+      </div>
+
+
+
+      
+    </div>
   )
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './sidebar.css'
+
 import { SlFeed } from "react-icons/sl";
 import { IoChatboxEllipsesSharp } from "react-icons/io5";
 import { PiVideoFill } from "react-icons/pi";
 import { MdGroups2 } from "react-icons/md";
+
 
 
 
@@ -48,48 +50,36 @@ export const Sidebar = () => {
             <MdGroups2/>
             <span>Groups</span>
         </div>
-        <div className="sidebaritems">
-
-            <MdGroups2/>
-            <span>Groups</span>
-        </div>
-        <div className="sidebaritems">
-
-            <MdGroups2/>
-            <span>Groups</span>
-        </div>
-        <div className="sidebaritems">
-
-            <MdGroups2/>
-            <span>Groups</span>
-        </div>
-        <div className="sidebaritems">
-
-            <MdGroups2/>
-            <span>Groups</span>
-        </div>
-        <div className="sidebaritems">
-
-            <MdGroups2/>
-            <span>Groups</span>
-        </div>
-        <div className="sidebaritems">
-
-            <MdGroups2/>
-            <span>Groups</span>
-        </div>
-        <div className="sidebaritems">
-
-            <MdGroups2/>
-            <span>Groups</span>
-        </div>
-    
+       
         <div className="">
 
             <button className='sidebarButton'>See More</button>
             <hr className='sidebarHr'/>
             
         </div>
+
+
+        <div className='sidebarFriendList'>
+            <img className='sidebarFriendImage' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEUZrYHlA1Omsmisn1UTL18o4pY-X1c6Jmlw&usqp=CAU" alt="" />
+            <span>Jack ma</span>
+
+        </div>
+        <div className='sidebarFriendList'>
+            <img className='sidebarFriendImage' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEUZrYHlA1Omsmisn1UTL18o4pY-X1c6Jmlw&usqp=CAU" alt="" />
+            <span>Elon Musk</span>
+
+        </div>
+        <div className='sidebarFriendList'>
+            <img className='sidebarFriendImage' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEUZrYHlA1Omsmisn1UTL18o4pY-X1c6Jmlw&usqp=CAU" alt="" />
+            <span>Billgates</span>
+
+        </div>
+        <div className='sidebarFriendList'>
+            <img className='sidebarFriendImage' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEUZrYHlA1Omsmisn1UTL18o4pY-X1c6Jmlw&usqp=CAU" alt="" />
+            <span>Ambani</span>
+
+        </div>
+       
     
         
     
